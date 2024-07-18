30ee57c2fb163b219e219710eb47b69da424f74a
yangfei9300 <yf_tiaotiao@163.com>
2024-07-18 14:56:04 +0800

    静态保存

---
+++ App - 副本 (2).vue
@@ -0,0 +1,42 @@
+<script>
+	export default {
+		globalData:{
+			activityId:5,
+		},
+		onLoad() {  
+		},
+		onLaunch: function() {
+			console.log('App Launch');
+		},
+		onShow: function() {	
+			console.log('App Show');
+		},
+		onHide: function() {   
+			console.log('App Hide');
+		},
+		methods:{
+			//AES加密
+			aesEncrypt(encrypt) {
+				//加密值
+				var e = this.AES.encrypt(encrypt, '1234567891234567', '1234567891234567')
+				// console.error("加密结果",e);
+				return e;
+			},
+			//AES解密
+			aesDecrypt(decrypt) {
+				// console.error("解密值",decrypt);
+				//解密值
+				var d = this.AES.decrypt(decrypt, '1234567891234567', '1234567891234567')
+				// console.error("解密结果",d);
+				return d;
+			},
+		}
+	}  
+</script>  
+
+<style  lang="scss">
+	/*每个页面公共css */  
+	@import url("/style/style.css");
+	@import url("/style/style_rpx.css");
+	@import "/style/ty_style_scss.scss";
+</style>
